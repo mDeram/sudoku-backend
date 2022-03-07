f43a5a2654e58fc0501569b484a0bbab01c82592
Deramecourt Mathurin <contact@mderam.com>
2022-03-07 17:53:12 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', () => {
+        /* … */
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
