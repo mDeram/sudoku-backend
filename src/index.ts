72aef215d165f326f1109552ba3f830fea7139b3
Deramecourt Mathurin <contact@mderam.com>
2022-03-07 18:44:57 +0100

    basic frontend ws connection

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-io.on('connection', () => {
-        /* … */
+const server = http.createServer(app);
+const io = new Server(server, { transports: ["websocket"] });
+
+io.on("connection", socket => {
+    console.log("new connection from: ", socket.handshake.url);
+    socket.emit("hello", "hello there");
+    socket.on("disconnect", () => {
+        console.log("disconnection from: ", socket.handshake.url);
+    });
 });
 
-const PORT = 3000;
+const PORT = 4000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
